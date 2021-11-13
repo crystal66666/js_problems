5a9762fc945ffb56d8c77f6cd0878243e6f5ff14
sleepy-potato <85094950+sleepy-potato@users.noreply.github.com>
2021-11-12 23:12:00 -0800

    Update flatten_array_depth.js

--- flatten_array_depth.js
+++ flatten_array_depth.js
@@ -1,35 +1,35 @@
 ////////////////////////////////////////////////
 // Recursive
 ////////////////////////////////////////////////
-function flatten_recursive_helper(arr, result) {
-	for (let i = 0; i < arr.length; i++) {
-  	const elem = arr[i];
-    if (Array.isArray(elem)) {
-    	flatten_recursive_helper(elem, result);
+function flatten_recursive_helper(arr, result, depth) {
+  for (let i = 0; i < arr.length; i++) {
+    const elem = arr[i];
+    if (Array.isArray(elem) && depth > 0) {
+      flatten_recursive_helper(elem, result, depth - 1);
     } else {
-    	result.push(elem);
+      result.push(elem);
     }
   }
 }
 
-function flatten_recursive(arr, depth) {
-	const result = [];
-	helper(arr, result);
+function flatten_recursive(arr, depth = 1) {
+  const result = [];
+  flatten_recursive_helper(arr, result, depth);
   return result;
 }
 
 ////////////////////////////////////////////////
 // Iterative with stack
 ////////////////////////////////////////////////
-function flatten_with_stack(arr, depth=1) {
-	const result = [];
- 	const stack = [...arr.map(elem => ([elem, depth]))];
+function flatten_with_stack(arr, depth = 1) {
+  const result = [];
+  const stack = [...arr.map(elem => ([elem, depth]))];
   while (stack.length > 0) {
-  	const [top, top_depth] = stack.pop();
+    const [top, top_depth] = stack.pop();
     if (Array.isArray(top) && top_depth > 0) {
-    	stack.push(...top.map(elem => ([elem, top_depth - 1])));
+      stack.push(...top.map(elem => ([elem, top_depth - 1])));
     } else {
-    	result.push(top);
+      result.push(top);
     }
   }
   return result.reverse();
