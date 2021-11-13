95d2f50f151596fb57af5ee877c3eed595f88472
sleepy-potato <85094950+sleepy-potato@users.noreply.github.com>
2021-11-12 22:54:20 -0800

    Create flatten_array.js

---
+++ flatten_array.js
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////
+// Recursive
+////////////////////////////////////////////////
+ function flatten_recursive_helper(arr, result) {
+	for (let i = 0; i < arr.length; i++) {
+  	const elem = arr[i];
+    if (Array.isArray(elem)) {
+    	flatten_recursive_helper(elem, result);
+    } else {
+    	result.push(elem);
+    }
+  }
+}
+
+function flatten_recursive(arr) {
+	const result = [];
+	helper(arr, result);
+  return result;
+}
+
+////////////////////////////////////////////////
+// Iterative with stack
+////////////////////////////////////////////////
+function flatten_with_stack(arr) {
+	const result = [];
+	const stack = [...arr];
+  while (stack.length > 0) {
+  	const top = stack.pop();
+    if (Array.isArray(top)) {
+    	stack.push(...top);
+    } else {
+    	result.push(top);
+    }
+  }
+  return result.reverse();
+}
